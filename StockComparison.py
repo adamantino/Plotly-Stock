8a0f036270c75251f01609f3bdd345a892c84972
Stefano <mothership@Stefanos-MacBook.local>
2015-05-29 00:08:16 +0200

    initial commit

---
+++ StockComparison.py
@@ -0,0 +1,105 @@
+import plotly.plotly as py
+from plotly.graph_objs import *
+
+
+import numpy as np
+import pandas as pd
+ewma = pd.stats.moments.ewma
+from pandas.io.data import DataReader
+import pytz
+from datetime import datetime
+from random import randint
+
+
+
+# Thanks to http://connor-johnson.com/2014/02/01/smoothing-with-exponentially-weighted-moving-averages/
+def holt_winters_second_order_ewma( x, span, beta ):
+    N = x.size
+    alpha = 2.0 / ( 1 + span )
+    s = np.zeros(( N, ))
+    b = np.zeros(( N, ))
+    s[0] = x[0]
+    for i in range( 1, N ):
+        s[i] = alpha * x[i] + ( 1 - alpha )*( s[i-1] + b[i-1] )
+        b[i] = beta * ( s[i] - s[i-1] ) + ( 1 - beta ) * b[i-1]
+    return s
+
+def ewma_double_directions(x, span):
+	fwd = ewma( y, span=15 ) # take EWMA in fwd direction
+	bwd = ewma( y[::-1], span=15 ) # take EWMA in bwd direction
+	avg = np.vstack(( fwd, bwd[::-1] )) # lump fwd and bwd together
+	return np.mean( avg, axis=0 ) # average
+
+
+
+all_data = {}
+traces = list()
+
+conversion = {
+    'Open': 'first', 
+    'High': 'max', 
+    'Low': 'min', 
+    'Close': 'last', 
+    'Volume': 'sum', 
+    'Adj Close': 'last'
+}
+
+##complete_sp500 = ['A', 'AA', 'AAPL', 'ABBV', 'ABC', 'ABT', 'ACE', 'ACN', 'ACT', 'ADBE', 'ADI', 'ADM', 'ADP', 'ADS', 'ADSK', 'ADT', 'AEE', 'AEP', 'AES', 'AET', 'AFL', 'AGN', 'AIG', 'AIV', 'AIZ', 'AKAM', 'ALL', 'ALLE', 'ALTR', 'ALXN', 'AMAT', 'AME', 'AMG', 'AMGN', 'AMP', 'AMT', 'AMZN', 'AN', 'ANTM', 'AON', 'APA', 'APC', 'APD', 'APH', 'ARG', 'ATI', 'AVB', 'AVGO', 'AVP', 'AVY', 'AXP', 'AZO', 'BA', 'BAC', 'BAX', 'BBBY', 'BBT', 'BBY', 'BCR', 'BDX', 'BEN', 'BF.B', 'BHI', 'BIIB', 'BK', 'BLK', 'BLL', 'BMY', 'BRCM', 'BRK.B', 'BSX', 'BWA', 'BXP', 'C', 'CA', 'CAG', 'CAH', 'CAM', 'CAT', 'CB', 'CBG', 'CBS', 'CCE', 'CCI', 'CCL', 'CELG', 'CERN', 'CF', 'CFN', 'CHK', 'CHRW', 'CI', 'CINF', 'CL', 'CLX', 'CMA', 'CMCSA', 'CME', 'CMG', 'CMI', 'CMS', 'CNP', 'CNX', 'COF', 'COG', 'COH', 'COL', 'COP', 'COST', 'COV', 'CPB', 'CRM', 'CSC', 'CSCO', 'CSX', 'CTAS', 'CTL', 'CTSH', 'CTXS', 'CVC', 'CVS', 'CVX', 'D', 'DAL', 'DD', 'DE', 'DFS', 'DG', 'DGX', 'DHI', 'DHR', 'DIS', 'DISCA', 'DISCK', 'DLPH', 'DLTR', 'DNB', 'DNR', 'DO', 'DOV', 'DOW', 'DPS', 'DRI', 'DTE', 'DTV', 'DUK', 'DVA', 'DVN', 'EA', 'EBAY', 'ECL', 'ED', 'EFX', 'EIX', 'EL', 'EMC', 'EMN', 'EMR', 'EOG', 'EQR', 'EQT', 'ESRX', 'ESS', 'ESV', 'ETFC', 'ETN', 'ETR', 'EW', 'EXC', 'EXPD', 'EXPE', 'F', 'FAST', 'FB', 'FCX', 'FDO', 'FDX', 'FE', 'FFIV', 'FIS', 'FISV', 'FITB', 'FLIR', 'FLR', 'FLS', 'FMC', 'FOSL', 'FOXA', 'FSLR', 'FTI', 'FTR', 'GAS', 'GCI', 'GD', 'GE', 'GGP', 'GILD', 'GIS', 'GLW', 'GM', 'GMCR', 'GME', 'GNW', 'GOOG', 'GOOGL', 'GPC', 'GPS', 'GRMN', 'GS', 'GT', 'GWW', 'HAL', 'HAR', 'HAS', 'HBAN', 'HCBK', 'HCN', 'HCP', 'HD', 'HES', 'HIG', 'HOG', 'HON', 'HOT', 'HP', 'HPQ', 'HRB', 'HRL', 'HRS', 'HSP', 'HST', 'HSY', 'HUM', 'IBM', 'ICE', 'IFF', 'INTC', 'INTU', 'IP', 'IPG', 'IR', 'IRM', 'ISRG', 'ITW', 'IVZ', 'JCI', 'JEC', 'JNJ', 'JNPR', 'JOY', 'JPM', 'JWN', 'K', 'KEY', 'KIM', 'KLAC', 'KMB', 'KMI', 'KMX', 'KO', 'KORS', 'KR', 'KRFT', 'KSS', 'KSU', 'L', 'LB', 'LEG', 'LEN', 'LH', 'LLL', 'LLTC', 'LLY', 'LM', 'LMT', 'LNC', 'LO', 'LOW', 'LRCX', 'LUK', 'LUV', 'LVLT', 'LYB', 'M', 'MA', 'MAC', 'MAR', 'MAS', 'MAT', 'MCD', 'MCHP', 'MCK', 'MCO', 'MDLZ', 'MDT', 'MET', 'MHFI', 'MHK', 'MJN', 'MKC', 'MLM', 'MMC', 'MMM', 'MNK', 'MNST', 'MO', 'MON', 'MOS', 'MPC', 'MRK', 'MRO', 'MS', 'MSFT', 'MSI', 'MTB', 'MU', 'MUR', 'MWV', 'MYL', 'NAVI', 'NBL', 'NBR', 'NDAQ', 'NE', 'NEE', 'NEM', 'NFLX', 'NFX', 'NI', 'NKE', 'NLSN', 'NOC', 'NOV', 'NRG', 'NSC', 'NTAP', 'NTRS', 'NU', 'NUE', 'NVDA', 'NWL', 'NWSA', 'OI', 'OKE', 'OMC', 'ORCL', 'ORLY', 'OXY', 'PAYX', 'PBCT', 'PBI', 'PCAR', 'PCG', 'PCL', 'PCLN', 'PCP', 'PDCO', 'PEG', 'PEP', 'PETM', 'PFE', 'PFG', 'PG', 'PGR', 'PH', 'PHM', 'PKI', 'PLD', 'PLL', 'PM', 'PNC', 'PNR', 'PNW', 'POM', 'PPG', 'PPL', 'PRGO', 'PRU', 'PSA', 'PSX', 'PVH', 'PWR', 'PX', 'PXD', 'QCOM', 'QEP', 'R', 'RAI', 'RCL', 'REGN', 'RF', 'RHI', 'RHT', 'RIG', 'RL', 'ROK', 'ROP', 'ROST', 'RRC', 'RSG', 'RTN', 'SBUX', 'SCG', 'SCHW', 'SE', 'SEE', 'SHW', 'SIAL', 'SJM', 'SLB', 'SNA', 'SNDK', 'SNI', 'SO', 'SPG', 'SPLS', 'SRCL', 'SRE', 'STI', 'STJ', 'STT', 'STX', 'STZ', 'SWK', 'SWN', 'SWY', 'SYK', 'SYMC', 'SYY', 'T', 'TAP', 'TDC', 'TE', 'TEG', 'TEL', 'TGT', 'THC', 'TIF', 'TJX', 'TMK', 'TMO', 'TRIP', 'TROW', 'TRV', 'TSCO', 'TSN', 'TSO', 'TSS', 'TWC', 'TWX', 'TXN', 'TXT', 'TYC', 'UA', 'UHS', 'UNH', 'UNM', 'UNP', 'UPS', 'URBN', 'URI', 'USB', 'UTX', 'V', 'VAR', 'VFC', 'VIAB', 'VLO', 'VMC', 'VNO', 'VRSN', 'VRTX', 'VTR', 'VZ', 'WAG', 'WAT', 'WDC', 'WEC', 'WFC', 'WFM', 'WHR', 'WIN', 'WM', 'WMB', 'WMT', 'WU', 'WY', 'WYN', 'WYNN', 'XEC', 'XEL', 'XL', 'XLNX', 'XOM', 'XRAY', 'XRX', 'XYL', 'YHOO', 'YUM', 'ZION', 'ZMH', 'ZTS']
+
+
+tickers = ['ISP.MI', 'UCG.MI', 'MED.MI', 'BMPS.MI']
+
+for ticker in tickers:
+	try:
+		all_data[ticker] = DataReader(ticker,  "yahoo", datetime(2010,1,1), datetime.utcnow().replace(tzinfo = pytz.utc))
+		
+		x = all_data[ticker].index
+		y = all_data[ticker]['Adj Close']
+		
+		c = holt_winters_second_order_ewma(y, 10, 0.3)
+#		c = ewma_double_directions(y, 10)
+
+		color = 'rgba('+str(randint(0,255))+','+str(randint(0,255))+','+str(randint(0,255))
+		
+		traces.append(Scatter(
+		     x=x,
+		     y=c,
+		     name=ticker + ' Moving Average',
+		     line=Line(
+		         color=color + ', 0.4)',
+		         width=2
+		     ),
+		 ))
+		
+		
+		# resampling
+		#		for key, df in all_data[ticker].items():
+		# 			all_data[ticker] = df.resample('W', how=conversion)
+		# 						
+		# 		x = all_data[ticker].index
+		# 		y = all_data[ticker]['Adj Close']
+		
+		traces.append(Scatter(
+		    x=x,
+		    y=y,
+		    mode='markers',
+		    name=ticker + ' Share Price',
+		    marker=Marker(
+		        color=color + ', 1)',
+		        size=3,
+		        opacity=0.8
+		    )
+		))
+				
+		
+	except:
+		print("Cant find ", ticker)
+
+
+data = Data(traces)
+
+fig = Figure(data=data)
+plot_url = py.plot(fig, world_readable=False)
+
+
+
